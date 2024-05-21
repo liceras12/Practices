585114b3ed3f5e4f8148d2814d3550c98f7711d4
liceras12 <103392072+liceras12@users.noreply.github.com>
2024-05-20 23:53:58 -0400

    partFive

---
+++ PartFive/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartFive
+{
+    public class Book
+    {
+        public string title { get; set; }
+        public int pages { get; set; }
+
+        public Book(string title, int pages)
+        {
+            this.title=title;
+            this.pages=pages;
+        }
+    }
+}
---
+++ PartFive/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PartFive;
+using System;
+using System.Collections.Generic;
+
+namespace Practices
+{
+    class Program
+    {
+        private Random random = new Random();
+        private Dictionary<string, List<Book>> library = new Dictionary<string, List<Book>>();
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+        }
+
+        public void AddBook(string genre, Book book)
+        {
+            if (!library.ContainsKey(genre))
+            {
+                library[genre] = new List<Book>();
+            }
+            library[genre].Add(book);
+        }
+    }
+}
